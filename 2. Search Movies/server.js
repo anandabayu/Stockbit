ecea169bf93db5452df937092bc5a6bbf0804739
Ananda Yudhistira <anandabayu12@gmail.com>
2021-10-21 15:02:32 +0700

    change port to dynamic heroku port

--- 2. Search Movies/server.js
+++ 2. Search Movies/server.js
@@ -4,14 +4,13 @@
  * File Created: Thursday, 21st October 2021 2:07:43 pm
  * Author: Ananda Yudhistira (anandabayu12@gmail.com)
  * -----
- * Last Modified: Thursday, 21st October 2021 2:09:23 pm
+ * Last Modified: Thursday, 21st October 2021 3:02:17 pm
  * Modified By: Ananda Yudhistira (anandabayu12@gmail.com>)
  * -----
  * Copyright 2021 Ananda Yudhistira, -
  */
 const app = require('./index');
-const port = 3000;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Search Movies app listening at http://localhost:${port}`);
 });
