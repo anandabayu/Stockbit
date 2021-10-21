8ad9cc116de1511a7b17d90a3856868604d45c60
Ananda Yudhistira <anandabayu12@gmail.com>
2021-10-21 15:05:09 +0700

    fixing port variable

--- 2. Search Movies/server.js
+++ 2. Search Movies/server.js
@@ -4,13 +4,14 @@
  * File Created: Thursday, 21st October 2021 2:07:43 pm
  * Author: Ananda Yudhistira (anandabayu12@gmail.com)
  * -----
- * Last Modified: Thursday, 21st October 2021 3:02:17 pm
+ * Last Modified: Thursday, 21st October 2021 3:05:00 pm
  * Modified By: Ananda Yudhistira (anandabayu12@gmail.com>)
  * -----
  * Copyright 2021 Ananda Yudhistira, -
  */
 const app = require('./index');
+const port = 3000;
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Search Movies app listening at http://localhost:${port}`);
 });
