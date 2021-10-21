545bd6681135b73941a2e1f980da8f8f3b7cd1c0
Ananda Yudhistira <anandabayu12@gmail.com>
2021-10-21 13:34:31 +0700

    add search movie

---
+++ 2. Search Movies/README.md
@@ -0,0 +1,19 @@
+## Run
+
+type this command to run:
+
+```Shell
+npm start
+```
+
+search movie:
+
+```Shell
+http://localhost:3000/movies?q={your_query}&page={page default 1}
+```
+
+detail movie:
+
+```Shell
+http://localhost:3000/movies/{imdbID get from search}
+```
---
+++ 2. Search Movies/index.js
@@ -0,0 +1,64 @@
+/*
+ * File: index.js
+ * Project: search-movies
+ * File Created: Thursday, 21st October 2021 12:04:36 pm
+ * Author: Ananda Yudhistira (anandabayu12@gmail.com)
+ * -----
+ * Last Modified: Thursday, 21st October 2021 12:37:57 pm
+ * Modified By: Ananda Yudhistira (anandabayu12@gmail.com>)
+ * -----
+ * Copyright 2021 Ananda Yudhistira, -
+ */
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const axios = require('axios');
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  res.send('Stockbit 👍👍👍');
+});
+
+app.get('/movies', async (req, res) => {
+  const { q, page } = req.query;
+
+  if (!q) {
+    res.json({
+      success: false,
+      message: 'Query is empty',
+    });
+  }
+
+  const response = await axios.get(`https://www.omdbapi.com`, {
+    params: {
+      apikey: 'faf7e5bb',
+      s: q,
+      page: !page ? 1 : page,
+    },
+  });
+  res.json(response.data);
+});
+
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const response = await axios.get(`https://www.omdbapi.com`, {
+    params: {
+      apikey: 'faf7e5bb',
+      i: id,
+    },
+  });
+  res.json(response.data);
+});
+
+app.use(function (req, res, next) {
+  res
+    .status(404)
+    .json({ success: false, message: 'the url you access is not found!' });
+});
+
+app.listen(port, () => {
+  console.log(`Search Movies app listening at http://localhost:${port}`);
+});
